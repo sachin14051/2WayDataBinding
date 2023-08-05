e661c3bc07743d66f7568afa91d05af9d273c705
sachin <sachin14051@gmail.com>
2023-08-05 23:47:11 +0530

    working on  two (2)data  binding

--- src/app/tab-c/tab-c.component.ts
+++ src/app/tab-c/tab-c.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab-c.component.scss']
 })
 export class TabCComponent implements OnInit {
-public text  !: string
   constructor() { }
-
+public name !: string;
+public text !:string
   ngOnInit(): void {
   }
 
